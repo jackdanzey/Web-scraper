18c7685454941fe3483f877b8547e3a0313f243f
Jack Danzey <jackdanzey@gmail.com>
2023-03-28 08:56:32 -0400

    testing some features

--- scraper.py
+++ scraper.py
@@ -1,2 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
+
+r = requests.get(
+    'https://www.reuters.com/article/us-health-coronavirus-global-deaths/global-coronavirus-deaths-pass-agonizing-milestone-of-1-million-idUSKBN26K08Y')
+
+data = r.text
+
+soup = BeautifulSoup(data, "html.parser")
+
+print(soup.prettify())
